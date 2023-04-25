2fc410f55f0379f2063d03bb3c58c0c40317f263
CTO <thepropertymanager2020@gmail.com>
2023-04-25 20:48:41 +0800

    added sample function codes

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,20 @@
 //import packages and other files
 require('dotenv').config();
-// console.log(process.env) // remove this after you've confirmed it is working
+//console.log(process.env) // remove this after you've confirmed it is working
 const express = require('express');
 const router = express.Router();
 const bodyParser =  require('body-parser');
 const { v4: uuidv4 } = require('uuid');
-const session = require('express-session');
+var  session = require("express-session");
 const bcrypt = require('bcrypt');
 const mysql = require('mysql');
+const { query, validationResult } = require('express-validator');
+
 
 const saltRounds = 10;
 // Store hash in your password DB.
 const salt = bcrypt.genSaltSync(saltRounds);
+
 
  // create application/json parser
 var jsonParser = bodyParser.json()
@@ -40,7 +43,7 @@ db.connect((err) => {
 app.use(session({
     secret: process.env.SESSION_SECRET_KEY,
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: false,
 }));
 
 //login page
@@ -49,7 +52,9 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-    res.render('login');
+    res.render('login',{
+        'error':req.app.get('error')
+    });
 });
 
 //signup page
@@ -58,23 +63,36 @@ router.get('/signup', (req, res) => {
 });
 
 //post signup
-router.post('/signup', urlencodedParser, (req, res) => {
+router.post('/signup',
+[
+    query('name').notEmpty().withMessage('Name cannot be empty.'),
+    query('username').notEmpty().withMessage('Username cannot be empty.'),
+    query('password').notEmpty().withMessage('Password cannot be empty.')
+]
+, urlencodedParser, (req, res) => {
      //access data from the form
     let name = req.body.name;
     let username = req.body.username;
     let password = req.body.password;
-
     //encrypt the password
     let encryptedPassword = bcrypt.hashSync(password, salt);
 
-    let user = {'name':name, 'username': username, 'password':encryptedPassword};
-    let sql = `INSERT INTO users SET ?`;
-    let query = db.query(sql, user, (err, result)=>{
+    const errors = validationResult(req)
+
+    console.log(errors.isEmpty())
+    
+    if(errors.isEmpty()){
+        let user = {'name':name, 'username': username, 'password':encryptedPassword};
+        let sql = `INSERT INTO users SET ?`;
+        let query = db.query(sql, user, (err, result)=>{
         if(err) throw err;
         res.render('dashboard',{
             'username': username,
         })
     });
+    }else{
+        return res.status(400).json({errors:errors.array()});
+    }
 });
 
 //authenticate user
@@ -91,50 +109,63 @@ router.post('/authenticate',  urlencodedParser, (req, res) => {
      
         let query = db.query(sql, params, (err, result)=> {
           if (err) {
-
              throw err;
-
           } else {
-
               if (result.length == 0) {
-
-                  res.send("Invalid username.");
-
+                req.app.set('error', 'Invalid username!')
+                res.redirect('/login')
               } else {
-                 
                   var hashedPassword = result[0].password;
                   var response = bcrypt.compareSync(password, hashedPassword);
 
                   if (response == false) {
-                     res.send("Password verification failed.");
+                      req.app.set('error', 'Password verification failed!')
+                      res.redirect('/login')
                   } else {
-                        //create a session here
-                        //code
+                        //session
+                        req.app.set('username', username)
                         res.redirect('/dashboard')
                   }
               }
           }
        });
 	} else {
-		res.send('Please enter Username and Password!');
-		res.end();
+        req.app.set('error', "Please enter username and password!")
+        res.redirect('/login')
 	}
 });
 
 //dashboard page
-router.get('/dashboard', urlencodedParser, (req, res) => {
-    // check if the use is authenticated via session
-    //code here
+router.get('/dashboard', (req, res) => {
     res.render('dashboard',{
-        'username':'username'
+        'username': req.app.get('username'),
     });
 });
 
 //logout 
 router.post('/logout' ,(req, res)=>{
     //destroy the created session
+    //code here
     res.render('login');
 });
+
+router.get('/add', (req, res)=>{
+    var sum = addNum(1,1)
+    console.log(sum+1)
+});
+
+//function
+function addNum(num1, num2){
+    const sum = num1 + num2;
+
+    return (sum)
+}
+
+function subtractNum(num1, num2){
+    var diff = num1 - num2;
+
+    return diff;
+}
 
 module.exports = router;
 
